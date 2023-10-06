3e6f3edc63523d07bb1805ee1d1c87c604b98d0f
Dhruv Agarwal <dhruvagarwal@Dhruvs-MacBook-Air.local>
2023-10-06 19:25:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { Calculator } from './components/Calculator';
+function App() {
+  return (
+    <div style={{border:"1px solid black",backgroundColor:"#00FFFF",widht:"50px",height:"800px"}}>
+      <center>
+        <Calculator/>
+      </center>
+    </div>
+  );
+}
+
+export default App;
